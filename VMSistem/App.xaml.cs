34c8f77aac5a89518e5abdcf691704caa9c6aa82
Davide Phirtskhalava <dato.istore@gmail.com>
2024-02-02 17:09:45 +0100

    Add project files.

---
+++ VMSistem/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace VMSistem
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ VMSistem/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VMSistem
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
